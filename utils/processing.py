118bd5a826b2c7485c6bb36c8f04c803c7fc73ad
gabrieljoanes <128034350+gabrieljoanes@users.noreply.github.com>
2025-05-15 18:05:15 +0300

    Update processing.py

--- utils/processing.py
+++ utils/processing.py
@@ -16,6 +16,9 @@ def get_transition_from_gpt(para_a, para_b, examples, client, model="gpt-4"):
         "entre deux paragraphes d'actualité régionale. "
         "La transition doit être journalistique, fluide, neutre et ne pas répéter les débuts comme 'Par ailleurs' ou parallèlement ou sujet."
         "the final TRANSITION in the article must be a proper concluding transition that clearly signals the end of the article. For that final transition only, choose from the following list of expressions: Enfin, Et pour finir, Pour terminer, Pour finir, En guise de conclusion, En conclusion, En guise de mot de la fin, Pour clore cette revue, Pour conclure cette sélection, Dernier point à noter, Pour refermer ce tour d’horizon. These closing transitions should only appear once and exclusively as the last transition in the article."
+        "if you use par ailleurs, c'est mieux d'étoffer, avec Par ailleurs, on annonce que, Par ailleurs, sachez que,"
+        "avoid the use of en parallèle"
+        
     )
 
     # Prepare messages for OpenAI chat completion
